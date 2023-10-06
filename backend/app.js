237233c3ca852758e5ec7f4000e010b6a0433f8a
Borrissytsch <Borrissytsch@yandex.ru>
2023-10-06 18:14:12 +0300

    Techincal version of Practicum 15th project (Sprint 15, Mesto full react-api-full-gha, 3rd part: CORS preflights added 4 localhost

--- backend/app.js
+++ backend/app.js
@@ -29,10 +29,19 @@ const allowedCors = [ // Массив доменов, с которых разр
 ];
 app.use((req, res, next) => {
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE'; // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const requestHeaders = req.headers['access-control-request-headers']; // сохраняем список заголовков исходного запроса
+
   if (allowedCors.includes(origin)) { // проверяем, что источник запроса есть среди разрешённых
     res.header('Access-Control-Allow-Origin', origin); // устанавливаем заголовок, разрешающий запросы с этого источника
   }
-  next();
+  if (method === 'OPTIONS') { // Если это предварительный запрос, добавляем нужные заголовки
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS); // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Headers', requestHeaders); // разрешаем кросс-доменные запросы с этими заголовками
+    return res.end(); // завершаем обработку запроса и возвращаем результат клиенту
+  }
+  return next();
 });
 /* ------ CORS middleware ends ------------ */
 
