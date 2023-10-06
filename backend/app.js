0ad087d581c070defd5edad6f32c1ad5a2667f0f
Borrissytsch <Borrissytsch@yandex.ru>
2023-10-07 01:40:50 +0300

    Techincal version of Practicum 15th project (Sprint 15, Mesto full react-api-full-gha, 3rd part: format time stamp 2 crash test

--- backend/app.js
+++ backend/app.js
@@ -53,14 +53,14 @@ app.use((req, res, next) => {
 /* ------ CORS middleware ends ------------ */
 
 /* Crash test middleware */
-// if (NODE_ENV.toLowerCase() !== 'production' || CRASH_TEST.toLowerCase() === 'on') {
-console.log(`${Date.now()}: Crash test ${CRASH_TEST} 4 mode ${NODE_ENV} started with path ${crashTestRoute}`);
-app.get(crashTestRoute, () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-// }
+if (NODE_ENV.toLowerCase() !== 'production' || CRASH_TEST.toLowerCase() === 'on') {
+  console.log(`${Date.now().getDate()}: Crash test ${CRASH_TEST} 4 mode ${NODE_ENV} started with path ${crashTestRoute}`);
+  app.get(crashTestRoute, () => {
+    setTimeout(() => {
+      throw new Error('Сервер сейчас упадёт');
+    }, 0);
+  });
+}
 app.use(requestLogger); // логгер запросов
 app.post('/signin', signJoiTest(), login);
 app.post('/signup', signJoiTest(), createUser);
