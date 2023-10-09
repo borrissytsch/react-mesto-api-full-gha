a29b6c22855b39ac3e6feaab27aacd7069252b33
Borrissytsch <Borrissytsch@yandex.ru>
2023-10-09 03:45:33 +0300

    Techincal version of Practicum 15th project (Sprint 15, Mesto full react-api-full-gha, 3rd part

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -12,7 +12,7 @@ constructor (loginData) {
     console.log(`Api got token 4 auth: ${localStorage.getItem('token')} 4 ${request} ${this._serverLogin(dir)}`);
     return fetch(this._serverLogin(dir), {method: request
       , headers: {'Content-Type': 'application/json'
-      , authorization: 'GET' ? localStorage.getItem('token') : `Bearer ${localStorage.getItem('token')}`
+      , Authorization: 'GET' ? localStorage.getItem('token') : `Bearer ${localStorage.getItem('token')}`
       // , Authorization: `Bearer ${localStorage.getItem('token')}`
       } // @ 08/10/2023 seek auth error
     }).then((res, msg = `${dir} autorize ${request} `) => handler(res, msg));
