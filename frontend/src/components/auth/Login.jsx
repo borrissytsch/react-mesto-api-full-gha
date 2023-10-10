c631da31805bb5d71d50dd8a7d1e6da9e7c4cd50
Borrissytsch <Borrissytsch@yandex.ru>
2023-10-10 19:00:34 +0300

    Techincal version of Practicum 15th project (Sprint 15, Mesto full react-api-full-gha, 3rd part: Login.jsx ret token done

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -19,7 +19,7 @@ export function Login ({loggedIn, startApp, handleToolTipOpen}) {
       if (result.token) {
         localStorage.setItem('token', result.token);
         console.log(`Login token is: ${result.token}`)
-        // return result.token; // commented @ 10/10/23 (why return if then will be done further)
+        return result.token;
       } else {
         console.log(`Token has not been received from server, result keys are: ${Object.keys(result).join('/')}`); // alert(`token has not been received from server, result keys are: ${Object.keys(result).join('/')}`)
       }
