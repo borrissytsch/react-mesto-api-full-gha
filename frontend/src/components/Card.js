c21df69b79c9b2e606ac8da38ebc92939378c2aa
Borrissytsch <Borrissytsch@yandex.ru>
2023-10-11 07:19:36 +0300

    Techincal version of Practicum 15th project (Sprint 15, Mesto full React-gha-auth, 3rd part: card like redraw, before 2nd code review

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -10,7 +10,7 @@ export default function Card({card, onCardClick, onCardLike, onCardDelete}) {
   const isOwn = card.owner === _id; // Trash search @ 10/10/23
   alert(`Cards like: _id ${_id} // likes: ${card.likes}`);
   // const isLiked = card.likes.some(i => i._id === _id);
-  const isLiked = card.likes.some(i => i === _id); // likes search @ 10/10/23
+  const isLiked = card.likes ? card.likes.some(i => i === _id) : false; // likes search @ 10/10/23
   // console.log(`Cards trash: isOwn/Liked-${isOwn}/${isLiked} 4 card ${Object.entries(card)} _id ${_id} of ${name}/${about} // owner: ${card.owner} & likes ${card.likes}`);
   function handleClick(evt) {
     onCardClick(evt, undefined, card);
