12490559aff80808ab37e0d6ad2003286b856741
Borrissytsch <Borrissytsch@yandex.ru>
2023-10-11 05:37:09 +0300

    Techincal version of Practicum 15th project (Sprint 15, Mesto full React-gha-auth, 3rd part: trash set, before 2nd code review

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -9,8 +9,8 @@ export default function Card({card, onCardClick, onCardLike, onCardDelete}) {
   // const isOwn = card.owner._id === _id;
   const isOwn = card.owner === _id; // Trash search @ 10/10/23
   const isLiked = card.likes.some(i => i._id === _id);
-  console.log(`Cards trash: isOwn/Liked-${isOwn}/${isLiked} 4 card ${Object.entries(card)} _id ${_id} of ${name}/${about} // owner: ${card.owner._id}`);
-  // alert(`Cards trash: isOwn/Liked-${isOwn}/${isLiked} 4 card ${Object.entries(card)} _id ${_id} of ${name}/${about} // owner: ${card.owner._id}`);
+  console.log(`Cards trash: isOwn/Liked-${isOwn}/${isLiked} 4 card ${Object.entries(card)} _id ${_id} of ${name}/${about} // owner: ${card.owner}`);
+  // alert(`Cards trash: isOwn/Liked-${isOwn}/${isLiked} 4 card ${Object.entries(card)} _id ${_id} of ${name}/${about} // owner: ${card.owner}`);
   function handleClick(evt) {
     onCardClick(evt, undefined, card);
   }
